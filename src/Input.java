7ed4f43402773264d57b7dc95dbfb0c6d0a73ebc
Orlandodlpz <143850773+Orlandodlpz@users.noreply.github.com>
2025-02-03 02:45:20 +0000

    implemented patient's input to get his/her name, age, weight, and if he smokes. Output was added.

--- src/Input.java
+++ src/Input.java
@@ -1,1 +1,27 @@
+import java.util.Scanner;
+public class Input {
 
+    public static void main (String [] alo){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("What is the patient's name? ");
+        String patient_name = scanner.nextLine();
+        System.out.print("What is the patient's age? ");
+        int patient_age = scanner.nextInt();
+        System.out.print("What is the patient's weight? ");
+        double patient_weight = scanner.nextDouble();
+        System.out.print("Does the patient smokes? ");
+        boolean patientS = scanner.nextBoolean();
+        scanner.close();
+        
+        System.out.println();        
+        System.out.println();
+        System.out.println("Patient's Info:");
+        System.out.println();
+        System.out.println("patient's name: " + patient_name);
+        System.out.println("patient's age: " + patient_age);
+        System.out.println("patient's weight: " + patient_weight);
+        System.out.println("Smoking Status: " + patientS);
+
+    }
+}
